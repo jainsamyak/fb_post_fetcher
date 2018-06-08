b5a3b1d97e71ca304ce5cb1b7ce5882e9a26c4dd
Samyak <prof_kamal@Kamals-iMac.local>
2018-06-08 12:42:34 +0530

    html added for view posts

--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,19 @@
 
-$(document).ready(function() {
-    $('#btn-set-pg').on('click',function(){
-        let ac_token=$('#txt_ac_token').val();
-        let pg_name=$('#txt_pg_name').val();
-        fetch_page(ac_token,pg_name);
+$(document).ready(function () {
+    $('#btn-set-pg').on('click', function () {
+        let ac_token = $('#txt_ac_token').val();
+        let pg_name = $('#txt_pg_name').val();
+        fetch_page(ac_token, pg_name);
+    });
+    $('#view_posts').on('click', function () {
+        const BrowserWindow = require('electron').remote.BrowserWindow
+        const path = require('path')
+
+        const modalPath = path.join('file://', __dirname, './view.html')
+        let win2 = new BrowserWindow({ frame: false })
+        win2.on('close', function () { win = null })
+        win2.loadURL(modalPath)
+        win2.show()
     });
 });
 
--- main.js
+++ main.js
@@ -1,29 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 const url = require('url')
 const path = require('path')
-var http = require('http')
-var fs = require('fs')
-//create a server object:
-//the server object listens on port 8080
 let win
 
 function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600 })
-    var http = require('http'),
-        fs = require('fs');
-
-
-    fs.readFile('./index.html', function (err, html) {
-        if (err) {
-            throw err;
-        }
-        http.createServer(function (request, response) {
-            response.writeHeader(200, { "Content-Type": "text/html" });
-            response.write(html);
-            response.end();
-        }).listen(8080);
-    });
-    win.loadURL('http://localhost:8080')
+    win = new BrowserWindow({ width: 900, height: 750 })
+    win.loadURL(url.format ({ 
+        pathname: path.join(__dirname, './index.html'), 
+        protocol: 'file:', 
+        slashes: true 
+     }))
+     win.webContents.openDevTools();
 }
 
 app.on('ready', createWindow) 
